57a08aa7d62a76e44d57301e019d49e877a9e83d
VITALII HURIN <raductionshop@gmail.com>
2026-04-26 12:32:25 +0300

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ProgressBars
+namespace ProgressBarT3
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -21,147 +21,42 @@ namespace ProgressBars
             InitializeComponent();
         }
 
-        private async Task FillPb(ProgressBar progressBar)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            Random rnd = new Random();
-
-            int randomNumber = rnd.Next(1, 10);
-
-            int i = 0;
-            while (true)
-            {
-                if (i + randomNumber <= 100)
+                if (!int.TryParse(FibonacciCountTextBox.Text, out int value) || value < 0)
                 {
-                    i += randomNumber;
-
-                    progressBar.Value = i;
-                    await Task.Delay(randomNumber * 100);
-
+                    MessageBox.Show("Будь ласка, введіть коректне ціле число (0 або більше).");
+                    return;
                 }
-                else
-                {
-                    int did = 100 - i;
-                    i += did;
 
-                    progressBar.Value = i;
-                    await Task.Delay(did * 100);
-                    break;
-                }
-            }
-        }
-        public void CreateProgressBar()
-        {
-            Random _rnd = new Random();
-            Color randomColor = Color.FromRgb(
-                (byte)_rnd.Next(0, 256),
-                (byte)_rnd.Next(0, 256),
-                (byte)_rnd.Next(0, 256)
-            );
-            ProgressBar progressBar = new ProgressBar
-            {
-                Minimum = 0,
-                Maximum = 100,
-                Value = 0,
-                Height = 20,
-                Margin = new Thickness(0, 5, 0, 5),
-                Foreground = new SolidColorBrush(randomColor)
-            };
-            ProgressBarContainer.Children.Add(progressBar);
-            _ = FillPb(progressBar);
-        }
+                await Task.Run(() => {
+                    List<long> fibonacciNumbers = new List<long>();
 
-        private async void Button_Click1(object sender, RoutedEventArgs e)
-        {
-            CreateProgressBar();
-            CreateProgressBar();
-        }
+                    fibonacciNumbers.Add(0);
+                    fibonacciNumbers.Add(1);
 
-        private async void Button_Click2(object sender, RoutedEventArgs e)
-        {
-            CreateProgressBar();
-            CreateProgressBar();
-            CreateProgressBar();
-        }
+                    long fib1 = 0;
+                    long fib2 = 1;
 
-        private async void Button_Click3(object sender, RoutedEventArgs e)
-        {
-            ProgressBarContainer.Children.Clear();
-        }
+                    long num = 0;
 
-        private int finishCounter = 0;
+                    while (num <= value)
+                    {
+                        num = fib1 + fib2;
+                        if (num <= value)
+                        {
+                            fibonacciNumbers.Add(num);
+                        }
+                        long temp = fib1;
+                        fib1 = fib2;
+                        fib2 = temp + fib2;
+                    }
 
-        private object lockObject = new object();
-
-        private List<string> _finishOrder = new List<string>();
-
-        Random rnd = new Random();
-
-        private async Task FillProgresBar(ProgressBar progressBar)
-        {
-            
-            int i = 0;
-            while (true)
-            {
-                int randomNumber = 0;
-                lock (lockObject)
-                {
-                    randomNumber = rnd.Next(1, 10);
-                }
-                
-                if (i + randomNumber <= 100)
-                {
-                    i += randomNumber;
-
-                    progressBar.Value = i;
-                    await Task.Delay(randomNumber * 100);
-
-                }
-                else
-                {
-                    int did = 100 - i;
-                    i += did;
-
-                    progressBar.Value = i;
-                    await Task.Delay(did * 100);
-                    break;
-                }
-            }
-            lock (lockObject)
-            {
-                if (progressBar.Value >= 100)
-                {
-                    finishCounter++;
-                    _finishOrder.Add(progressBar.Name);
-                }
-            }
-        }
-
-        private async void Start(object sender, RoutedEventArgs e)
-        {
-            var tasks = new List<Task>();
-            tasks.Add(FillProgresBar(Hourse1));
-            tasks.Add(FillProgresBar(Hourse2));
-            tasks.Add(FillProgresBar(Hourse3));
-            tasks.Add(FillProgresBar(Hourse4));
-            tasks.Add(FillProgresBar(Hourse5));
-
-            await Task.WhenAll(tasks);
-
-            string message = "";
-
-            foreach (var item in _finishOrder)
-            {   
-                message += item + "\n";
-            }
-            MessageBox.Show(message);
-
-            Hourse1.Value = 0;
-            Hourse2.Value = 0;
-            Hourse3.Value = 0;
-            Hourse4.Value = 0;
-            Hourse5.Value = 0;
-
-            _finishOrder.Clear();
+                    Dispatcher.Invoke(() =>
+                    {
+                        res.Text = fibonacciNumbers.Count.ToString();
+                    });
+                });
         }
     }
 }
